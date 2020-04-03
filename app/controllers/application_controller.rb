a1bf0a9942e235bf23088da545e1b5ea154a8588
Brian Nicholls <nichol88@gmail.com>
2020-04-03 19:00:49 -0400

    bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  
+
 
   def logged_in?
     !!current_user
@@ -15,4 +15,5 @@ class ApplicationController < ActionController::Base
 
   helper_method :redirect_if_not_logged_in
   helper_method :current_user
+  helper_method :logged_in?
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,4 +2,8 @@ class StaticController < ApplicationController
   def welcome
     #render :welcome
   end
+
+  def leaderboard
+    @users = User.all
+  end
 end

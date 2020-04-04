be1872cbc4c274f002124e1bd86d829e46da9c89
Brian Nicholls <nichol88@gmail.com>
2020-04-04 18:25:52 -0400

    load javascript after turbolinks:load

--- app/javascript/custom/form.js
+++ app/javascript/custom/form.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('turbolinks:load', () => {
   const buttons = document.getElementsByClassName('plus-minus')
   addListenersToButtons(buttons)
 })

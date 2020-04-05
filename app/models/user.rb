ca6dac41c5dc17e6c401ebe35011d3fb230fa2c2
Brian Nicholls <nichol88@gmail.com>
2020-04-05 19:20:02 -0400

    validate presence of user fname/lname

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :entries
   has_many :counters, through: :entries
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+
   def name
     "#{first_name} #{last_name}"
   end

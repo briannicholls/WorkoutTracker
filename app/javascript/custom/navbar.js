151e8cef8d686e8603d391481219426c478422f7
Brian Nicholls <nichol88@gmail.com>
2020-04-05 21:52:42 -0400

    add +-5 buttons

--- app/javascript/custom/navbar.js
+++ app/javascript/custom/navbar.js
@@ -1,9 +1,6 @@
 document.addEventListener('turbolinks:load', () => {
-  // const buttons = document.getElementsByClassName('plus-minus')
-  // addListenersToButtons(buttons)
   const btns = document.getElementsByClassName('leaderboard-buttons')
   for (let button of btns) {
-    console.log('iterating through buttons....')
     if(window.location.href == button.href){
       button.classList.add('w3-yellow')
     }
